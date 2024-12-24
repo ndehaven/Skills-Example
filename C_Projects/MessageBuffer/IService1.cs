e32635eb483571894637197ba852563fa03e7cec
ndehaven <106265519+ndehaven@users.noreply.github.com>
2024-12-24 14:10:29 -0700

    Messaging Buffer Project
    This project is centered around C programming language and depicts a messaging system that can be implemented into an existing framework or project where a buffer is made between messenger and messenger receiver and is passed between the two through a list of user interactions and commands.

---
+++ C_Projects/MessageBuffer/IService1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// Service Location http://localhost:53062/Service1.svc 
+
+namespace MessageBuffer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        // Servce Method Headers including WebGets for http responses 
+        [OperationContract]
+        [WebGet(UriTemplate = "/SendMsg?message={message}&recvID={recvID}&sendID={sendID}")]
+        string SendMsg(string message, string recvID, string sendID);
+        
+        [OperationContract]
+        [WebGet(UriTemplate = "/ReceiveMsg?recvID={recvID}&purge={purge}")]
+        string ReceiveMsg(string recvID, bool purge);
+        
+    }
+}
---
+++ C_Projects/MessageBuffer/Service1.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+using System.ServiceModel.Activation;
+using System.Web.Http.Cors;
+
+// Service Location http://localhost:53062/Service1.svc 
+
+namespace MessageBuffer
+{   // Needed to add this due to CORS error problems happening on the second service call 
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class Service1 : IService1
+    {
+        public string SendMsg(string message, string recvID, string sendID)
+        {
+            // Find xml path 
+            string xmlPath = @"C:\Users\16024\source\repos\MessageBuffer\MessageBuffer\XMLFile1.xml";
+            XDocument xmlFile; 
+            if(!File.Exists(xmlPath)){
+                xmlFile = new XDocument(new XElement("Messages"));
+            }
+            else{
+                // Load xml file containing database 
+                xmlFile = XDocument.Load(xmlPath);
+            }
+            // Set elements up to be added and saved to xml file using user input 
+            string time = DateTime.Now.ToString("HH:mm:ss");
+            XElement newMessage = new XElement("message",
+                        new XElement("messageString", message),
+                        new XElement("recvID", recvID), // Assuming sender ID is hardcoded for now
+                        new XElement("senderID", sendID), // Assuming receiver ID is hardcoded for now
+                        new XElement("timestamp", time));
+            xmlFile.Root.Add(newMessage);
+            xmlFile.Save(xmlPath); 
+            return $"Message '{message}' sent from {sendID} to {recvID}";
+        }
+
+        // Second RESTful service that will receive the message from the xml database that was buffered
+        public string ReceiveMsg(string recvID, bool purge)
+        {
+            // Find file of xml database 
+            string filePath = @"C:\Users\16024\source\repos\MessageBuffer\MessageBuffer\XMLFile1.xml";
+            XDocument doc = XDocument.Load(filePath);
+
+            // Query for message elements with the specified receiver ID
+            IEnumerable<string> messages = doc.Root.Elements("message")
+                                  .Where(m => m.Element("recvID").Value == recvID)
+                                  .Select(m => m.Element("messageString").Value);
+
+            string xmlResponse = "<ArrayOfstring>";
+            // STart point for messages 
+            foreach (string message in messages)
+            {
+                xmlResponse += $"<string>{message}</string>";
+            }
+            // End point of messages 
+            xmlResponse += "</ArrayOfstring>";
+            // Check purge value if needed to delete the previous messages 
+            if (purge)
+            {
+                doc.Root.Elements("message")
+                        .Where(m => m.Element("recvID").Value == recvID)
+                        .Remove();
+                doc.Save(filePath);
+            }
+
+            return xmlResponse;
+        }
+
+    }
+}
